6e1ecf7357b85e456b574a6dca26e3df7e7bb3f8
powtzulist <delasanarwin@gmail.com>
2024-03-29 04:10:22 +0800

    added particle js effect

---
+++ src/components/particles.vue
@@ -0,0 +1,95 @@
+<template>
+  <div id="app">
+    <vue-particles
+      id="tsparticles"
+      :particlesLoaded="particlesLoaded"
+      url="http://foo.bar/particles.json"
+    />
+
+    <vue-particles
+      id="tsparticles"
+      :particlesLoaded="particlesLoaded"
+      :options="{
+        background: {
+          color: {
+            value: '#212529',
+          },
+        },
+        fullScreen: {
+          enable: true,
+          zIndex: -1,
+        },
+        fpsLimit: 120,
+        interactivity: {
+          events: {
+            onClick: {
+              enable: true,
+              mode: 'push',
+            },
+            onHover: {
+              enable: true,
+              mode: 'repulse',
+            },
+          },
+          modes: {
+            bubble: {
+              distance: 400,
+              duration: 2,
+              opacity: 0.8,
+              size: 40,
+            },
+            push: {
+              quantity: 4,
+            },
+            repulse: {
+              distance: 200,
+              duration: 0.4,
+            },
+          },
+        },
+        particles: {
+          color: {
+            value: '#ffffff',
+          },
+          links: {
+            color: '#ffffff',
+            distance: 150,
+            enable: true,
+            opacity: 0.5,
+            width: 1,
+          },
+          move: {
+            direction: 'none',
+            enable: true,
+            outModes: 'bounce',
+            random: false,
+            speed: 6,
+            straight: false,
+          },
+          number: {
+            density: {
+              enable: true,
+            },
+            value: 80,
+          },
+          opacity: {
+            value: 0.5,
+          },
+          shape: {
+            type: 'circle',
+          },
+          size: {
+            value: { min: 1, max: 5 },
+          },
+        },
+        detectRetina: true,
+      }"
+    />
+  </div>
+</template>
+
+<script setup>
+const particlesLoaded = async (container) => {
+  console.log("Particles container loaded", container);
+};
+</script>
