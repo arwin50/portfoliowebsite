30407d648217908f377d23b9b4295420f42b1d06
powtzulist <delasanarwin@gmail.com>
2024-06-11 20:08:31 +0800

    added resume

--- src/components/Navigation/NavigationBar.vue
+++ src/components/Navigation/NavigationBar.vue
@@ -2,9 +2,9 @@
   <header class="h-[10%]   ">
     <nav class="w-full flex text-white px-2 justify-between pt-6 items-center">
      <span class="text-3xl ml-40 tracking-widest"><a href="#about">&#123 niw &#125</a></span>
-      <ul class="flex text-lg mr-52 space-x-10 tracking-wide  ">
+      <ul class="flex text-lg mr-52 space-x-10 tracking-wide items-center  ">
         <li class="hover:text-[#ffe169] "><a href="#projects" class="hover:shadow p-2 rounded-lg">projects</a></li>
-        <li class="hover:text-[#ffe169]"><a href="#" class="hover:shadow p-2 rounded-lg">resume</a></li>
+        <li class="hover:text-[#ffe169]"><button onclick="window.open('./resume.pdf','_blank')" class="hover:shadow p-2 rounded-lg">resume</button></li>
       </ul>
     </nav>
   </header>
